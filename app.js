30358519cdc06d52e3988671401366218e66df32
MrAlko99 <a.coluzzi99@gmail.com>
2021-06-20 15:09:09 +0200

    Model implentation

--- app.js
+++ app.js
@@ -1,33 +1,84 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://threejs.org/build/three.module.js';
+import {GLTFLoader} from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
+import {OBJLoader} from 'https://threejs.org/examples/jsm/loaders/OBJLoader.js';
+import {MTLLoader} from 'https://threejs.org/examples/jsm/loaders/MTLLoader.js';
+import {OrbitControls} from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
-let camera, scene, renderer;
+
+var camera, scene, renderer, controls;
 let mesh;
 
-init();
-animate();
-
 function init() {
-
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.z = 400;
-
   scene = new THREE.Scene();
+  scene.bacground = new THREE.Color('white');
 
-  const texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+  const light = new THREE.PointLight( 0xffffff, 50, 200 );
+  light.position.set(0, 200, 0);
+  const light1 = new THREE.PointLight( 0xffffff, 4, 200 );
+  light1.position.set(0, -50, 0);
+  scene.add(light);
+  scene.add(light1);
 
-  const geometry = new THREE.BoxGeometry( 200, 200, 200 );
-  const material = new THREE.MeshBasicMaterial( { map: texture } );
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load('./model/virus.glb',
+  function ( gltf ) {
+    const virusMesh = gltf.scene.children.find((child) => child.name === "virus");
+    virusMesh.scale.set(virusMesh.scale.x * 0.4, virusMesh.scale.y * 0.4, virusMesh.scale.z * 0.4);
+    var textureLoader = new THREE.TextureLoader();
+    textureLoader.load( "./model/Texture.png", function ( map ) {
+        virusMesh.material.map = map;
+        virusMesh.material.map.encoding = THREE.sRGBEncoding;
+        virusMesh.material.map.flipY = false;
+        virusMesh.material.needsUpdate = true;
+    });
+    virusMesh.position.y = virusMesh.scale.y;
+    virusMesh.rotation.y = -80;
 
-  mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
+    virusMesh.geometry.computeBoundingBox();
+    console.log(virusMesh.geometry.boundingBox);
+
+    scene.add(virusMesh);
+  },
+  function ( xhr ) {
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  },
+  function ( error ) {
+    console.log( 'An error happened' );
+  });
+
+  const mtlLoader = new MTLLoader();
+  const objLoader = new OBJLoader();
+  mtlLoader.load('./model/materials.mtl', (mtl) => {
+    mtl.preload();
+    objLoader.setMaterials(mtl);
+    objLoader.load('./model/Syringe.obj', (root) => {
+      console.log(root.scale.x);
+      root.scale.x=200;
+      root.scale.y=200;
+      root.scale.z=200;
+      scene.add(root);
+    });
+  });
 
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
+  /* CAMERA STUFF - Make function*/
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  window.addEventListener( 'resize', onWindowResize );
+  camera.position.y = 0;
+  camera.position.z = 50;
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(4.5, 0, 4.5);
+  controls.enablePan = true;
+  controls.maxPolarAngle = Math.PI / 2;
+  controls.enableDamping = true;
+
+  window.requestAnimationFrame(animate);
+  //window.addEventListener( 'resize', onWindowResize );
 
 }
 
@@ -44,9 +95,12 @@ function animate() {
 
   requestAnimationFrame( animate );
 
-  mesh.rotation.x += 0.005;
-  mesh.rotation.y += 0.01;
+  //mesh.rotation.x += 0.005;
+  //mesh.rotation.y += 0.01;
 
   renderer.render( scene, camera );
 
 }
+window.addEventListener('load',init);
+window.addEventListener('resize', onWindowResize);
+
