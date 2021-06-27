076fbd2eead52337c5211cfe106f61826145d7ca
Federica Cocci <federica.cocci99@gmail.com>
2021-06-27 23:30:57 +0200

    Deleating some comments

--- function/interactionObj.js
+++ function/interactionObj.js
@@ -2,37 +2,32 @@ import * as THREE from 'https://threejs.org/build/three.module.js';
 
 export function spreadingObj(nObject, objectMesh, noPlayingField, scene){
   var objectsArray = [];
-  // Create an array of syringes
   for(var i = 0; i<nObject; i++){
     objectsArray.push(objectMesh.clone());
   }
 
-  // Spread syringes giving different coordinates looking at the forbidden ones
   for(var i = 0; i<nObject; ){
-    var max = 500; //500 because the outsider room is 1000x1000
+    var max = 500;
     var min = -500;
     var x = Math.random() * (max - min) + min;
     var z = Math.random() * (max - min) + min;
     var values_ok;
     for(var j = 0; j<noPlayingField.length; j++){
-      var x_in = noPlayingField[j]['x_min'] <= x && x <= noPlayingField[j]['x_max'];  //x coordinates inside the obstacle
-      var z_in = noPlayingField[j]['z_min'] <= z && z <= noPlayingField[j]['z_max'];  //z coordinates inside the obstacle
-      if(!(x_in && z_in)){  //x and z outside the obstacle
-        values_ok = true;// DEVO CONTROLLARE SE è ANCHE FUORI AGLI ALTRI OSTACOLI
+      var x_in = noPlayingField[j]['x_min'] <= x && x <= noPlayingField[j]['x_max'];
+      var z_in = noPlayingField[j]['z_min'] <= z && z <= noPlayingField[j]['z_max'];
+      if(!(x_in && z_in)){
+        values_ok = true;
       }
       else{
         values_ok = false;
         break;
       }
     }
-    if(values_ok){  //vuol dire che è fuori a tutti gli ostacoli
-      objectsArray[i].position.set(x,55,z); //accetto questi valori e inserisco una siringa li
-      //objectsArray[i].rotation.set(Math.PI/6,0,Math.PI/6);
-      i++; //passo alla siringa successiva
+    if(values_ok){
+      objectsArray[i].position.set(x,55,z);
+      i++;
     }
     else{
-      //vuol dire che ho trovato un ostacolo in cui cadrebbe la siringa e quindi non posso accettare quei valori
-      //devo rigenerare altri due valori per inserire la siringa
     }
   }
   for(var i = 0; i<nObject; i++){
@@ -68,8 +63,6 @@ export function interactionPlayerObject(objectsArray, playerX, playerZ, aliveObj
         objectsArray[i].visible = false;
         aliveObjects = aliveObjects-1;
         var caughtObjects = objectsArray.length-aliveObjects;
-        // if(objectsArray[0].userData.tag == 'syringeEmpty') document.getElementById('syringeEmpty').innerHTML = " EMPTY &#128137 x " + caughtObjects;
-        // if(objectsArray[0].userData.tag == 'syringeFull') document.getElementById('syringeFull').innerHTML = "FULL &#128137 x " + caughtObjects;
         if(objectsArray[0].userData.tag == 'virus') document.getElementById('virus').innerHTML = "&#129440 x " + caughtObjects;
         if(objectsArray[0].userData.tag == 'gel') document.getElementById('gel').innerHTML = "&#129524 x " + caughtObjects;
         if(objectsArray[0].userData.tag == 'mask') document.getElementById('mask').innerHTML = "&#128567 x " + caughtObjects;
@@ -122,7 +115,7 @@ export function checkNearVirus(virus,playerX,playerZ,ray){
 export function contactWithVirus(virus, player, remainingLive){
   var inside = checkNearVirus(virus, player.position.x, player.position.z, 30);
   if(inside){
-      remainingLive -= 0.05*inside; //sono vicino al virus: o scappo, o lo uccido, o perdo vito
+      remainingLive -= 0.05*inside;
       document.getElementById("contact").innerHTML = "&#128156 " + Math.round(remainingLive) + "%";
   }
   return remainingLive;
